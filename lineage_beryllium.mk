3106d87659121c85e5d618d073f583817fe4e842
Rsyd1997 <rasyid2007alkautsar@gmail.com>
2023-09-02 09:21:28 +0000

    beryllium: LineageOS Bringing Up!

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -6,11 +6,14 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Havoc stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+WITH_SU := false
+LINEAGE_BUILDTYPE := EXPERIMENTAL
+WITH_TWRP := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_beryllium
+PRODUCT_NAME := lineage_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
