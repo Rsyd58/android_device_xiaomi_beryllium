70824517579b3c2922475bd6bc1c8e255d0de625
Rsyd <rasyid2007alkautsar@gmail.com>
2023-09-06 17:44:48 +0700

    Update lineage_beryllium.mk

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -8,9 +8,6 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-WITH_SU := false
-LINEAGE_BUILDTYPE := EXPERIMENTAL
-WITH_TWRP := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_beryllium
